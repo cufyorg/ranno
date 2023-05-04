911a216df177c9659ee13870bc8dd4b9e95c3a6b
Sulaiman Oboody <lsafer@cufy.org>
2023-05-04 17:59:52 +0300

    feat(ext-ktor)
    - added regex based filter
    - renamed `EnumeratedRoute.value` to `EnumeratedRoute.name`
    - renamed `EnumeratedApplication.value` to `EnumeratedApplication.name`
    - reformat code

--- ext-ktor/src/main/kotlin/RannoKtor.kt
+++ ext-ktor/src/main/kotlin/RannoKtor.kt
@@ -18,8 +18,9 @@ package org.cufy.ranno.ktor
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.cufy.ranno.*
+import org.intellij.lang.annotations.Language
 import kotlin.reflect.KCallable
-import kotlin.reflect.KFunction
+import kotlin.reflect.full.findAnnotations
 
 //////////////////////////////////////////////////
 
@@ -42,7 +43,7 @@ annotation class EnumeratedRoute(
      *
      * @since 1.0.0
      */
-    val value: String = "",
+    val name: String = "",
     /**
      * Used to reduce conflict between multiple modules.
      *
@@ -65,9 +66,7 @@ annotation class EnumeratedRoute(
  * @param arguments additional arguments.
  * @since 1.0.0
  */
-fun Route.applyEnumeratedRoute(
-    vararg arguments: Any?
-): List<Any?> {
+fun Route.applyEnumeratedRoute(vararg arguments: Any?): List<Any?> {
     return applyWith<EnumeratedRoute>(*arguments)
 }
 
@@ -86,11 +85,36 @@ fun Route.applyEnumeratedRoute(
  * @param arguments additional arguments.
  * @since 1.0.0
  */
+fun Route.applyEnumeratedRoute(vararg arguments: Any?, predicate: (KCallable<*>) -> Boolean): List<Any?> {
+    return applyWith<EnumeratedRoute>(*arguments, predicate = predicate)
+}
+
+/**
+ * Call all the functions and properties annotated with [EnumeratedRoute]
+ * and matches the given [name] regexp and [domain] regexp
+ * with [this] as the argument.
+ *
+ * Only functions and properties that can be invoked with the
+ * provided arguments will be invoked.
+ *
+ * ___Note: Only the elements passed to ranno
+ * annotation processor will be returned by this
+ * function.___
+ *
+ * @param arguments additional arguments.
+ * @since 1.0.0
+ */
 fun Route.applyEnumeratedRoute(
     vararg arguments: Any?,
-    predicate: (KCallable<*>) -> Boolean
+    @Language("RegExp") name: String = ".*",
+    @Language("RegExp") domain: String = ".*"
 ): List<Any?> {
-    return applyWith<EnumeratedRoute>(*arguments, predicate = predicate)
+    val n = name.toRegex()
+    val d = domain.toRegex()
+    return applyWith<EnumeratedRoute>(*arguments) {
+        it.findAnnotations<EnumeratedRoute>()
+            .any { it.name matches n && it.domain matches d }
+    }
 }
 
 //////////////////////////////////////////////////
@@ -114,7 +138,7 @@ annotation class EnumeratedApplication(
      *
      * @since 1.0.0
      */
-    val value: String = "",
+    val name: String = "",
     /**
      * Used to reduce conflict between multiple modules.
      *
@@ -137,9 +161,7 @@ annotation class EnumeratedApplication(
  * @param arguments additional arguments.
  * @since 1.0.0
  */
-fun Application.applyEnumeratedApplication(
-    vararg arguments: Any?
-): List<Any?> {
+fun Application.applyEnumeratedApplication(vararg arguments: Any?): List<Any?> {
     return applyWith<EnumeratedApplication>(*arguments)
 }
 
@@ -158,11 +180,36 @@ fun Application.applyEnumeratedApplication(
  * @param arguments additional arguments.
  * @since 1.0.0
  */
+fun Application.applyEnumeratedApplication(vararg arguments: Any?, predicate: (KCallable<*>) -> Boolean): List<Any?> {
+    return applyWith<EnumeratedApplication>(*arguments, predicate = predicate)
+}
+
+/**
+ * Call all the functions and properties annotated with [EnumeratedApplication]
+ * and matches the given [name] regexp and [domain] regexp
+ * with [this] as the argument.
+ *
+ * Only functions and properties that can be invoked with the
+ * provided arguments will be invoked.
+ *
+ * ___Note: Only the elements passed to ranno
+ * annotation processor will be returned by this
+ * function.___
+ *
+ * @param arguments additional arguments.
+ * @since 1.0.0
+ */
 fun Application.applyEnumeratedApplication(
     vararg arguments: Any?,
-    predicate: (KCallable<*>) -> Boolean
+    @Language("RegExp") name: String = ".*",
+    @Language("RegExp") domain: String = ".*"
 ): List<Any?> {
-    return applyWith<EnumeratedApplication>(*arguments, predicate = predicate)
+    val n = name.toRegex()
+    val d = domain.toRegex()
+    return applyWith<EnumeratedApplication>(*arguments) {
+        it.findAnnotations<EnumeratedApplication>()
+            .any { it.name matches n && it.domain matches d }
+    }
 }
 
 //////////////////////////////////////////////////
